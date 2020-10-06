d325f059b6f5f2e4e2e715b4da8d75f9a73bf69e
Ilya Vlasov <Fox20073@users.noreply.github.com>
2020-10-06 08:39:54 -0400

    Script update
    Script got updated and optimized, no extra comments and lines

--- script.js
+++ script.js
@@ -1,47 +1,5 @@
-// JavaScript repeated bu jQuery
-
 function NewItem(){
-// JS
-// Adding a new item to do 
-
-    // let li = document.createElement("li");
-    // let inputValue = document.getElementById("input").value;
-    // let text = document.createTextNode(inputValue);
-    // li.appendChild(text);
-
-    // if (inputValue === '') {
-    //     alert ("You must write something");
-    // } else {
-    //     let list = document.querySelector('#item');
-    //     list.appendChild(li);
-    // }
-
-// crossing out task
-
-    // function crossOut() {
-    //     li.classList.toggle("strike");
-    // }
-
-    // li.addEventListener("dblclick", crossOut);
-
-
-
-// Adding the "X" button
-
-    // let crossOutButton = document.createElement("X");
-    // crossOutButton.appendChild(document.createTextNode("X"));
-    // li.appendChild(crossOutButton);
-
-    // crossOutButton.addEventListener("click", deleteListItem);
-
-// Add class delete
-    // function deleteListItem(){
-    //     li.classList.add("delete")
-    // } 
-
-
-
-// jQ
+    
 // Adding a new item to do 
     let li = $("<li></li>");
     let inputValue = $("#input").val();
